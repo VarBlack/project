00167e86f030bf44c55488c22e32155aac648635
fortress-fighting <dreamersfly@163.com>
2016-12-20 00:36:43 +0800

    调整思路，简化代码第一次

---
+++ resume/js/index.js
@@ -0,0 +1,405 @@
+(function () {
+	// 主屏背景图 相关事件
+	function setMainImage (id) {
+		this.obj = $(id);
+		this.bg = $('.bg');
+		this.face = $('.face');
+		this.nowIndex = 0;
+		this.setting = {
+			data: [
+				'img/glasses_guy.jpg',
+				'img/aa_woman.jpg',
+				'img/young_girl.jpg',
+				'img/indian_guy2.jpg'
+				],
+			bgData: [
+				'img/bg.jpg',
+				'img/bgWhite.jpg'
+			],
+			len: 12,
+			bgSize: [1920, 1200],
+			bgRota: 1920 / 1200,
+			hideFn: function () {
+				return false;
+			},
+			showFn: function () {
+				return false;
+			},
+			isReady: false,
+			title: [
+				'HTML',
+				'css',
+				'JavaScript',
+				'Canvas'
+			],
+			infor: [
+				'At Adison Partners, we know what uncommon talent looks like',
+				'We are the ONLY retained firm that truly bets on our performance',
+				'Dramatically improves business performance outcomes by effectively leveraging people, processes, and technology',
+				'‘Disruptive Talent’ describes the brilliant individuals who think and act differently'
+			]
+		};
+		this.init();
+	}
+	setMainImage.prototype = {
+		constructor: setMainImage,
+		init: function (json) {
+			for (attr in json) {
+				this.setting[attr] = json[attr];
+			}
+			this.currentBg = this.setting.data;
+		},
+		tabBg: function (type) {
+			if (type) {
+				this.currentBg = this.setting.bgData;
+			} else {
+				this.currentBg = this.setting.data;
+			}
+			this.nowIndex = -1;
+		},
+		setPos: function (fn) {
+			// console.log(fn);
+			this.setLayOut(this.bg);
+			this.setLayOut(this.face);
+			fn && fn(this.setting.title[0],this.setting.infor[0])
+		},
+		rePos: function () {
+			this.setStyle(this.bg);
+			this.setStyle(this.face);
+		},
+		setLayOut: function (imgArea) {
+			var str = '';
+			for (var i = 0; i < 12; i++) {
+				str += '<div></div>'
+			}
+			imgArea.html(str);
+			this.setStyle(imgArea);
+			this.hideImage();
+		},
+		setStyle: function (imgArea) {
+			var _width = imgArea.find('div').width();
+			var _parWidth = imgArea.width();
+			var _parHeight = imgArea.height();
+
+			if (_parHeight*this.setting.bgRota > _parWidth) {
+				imgArea.css({
+					'min-width':_parHeight*this.setting.bgRota
+				});
+			};
+
+			// // 测试+
+			// if (_parWidth/this.setting.bgRota > _parHeight) {
+			// console.log(_parWidth/this.setting.bgRota)
+			// 	$('#mainImagArea').css({
+			// 		'min-height': _parWidth/this.setting.bgRota
+			// 	});
+			// };
+
+			_width = imgArea.find('div').width();
+
+			_parWidth = imgArea.width();
+
+			imgArea.find('div').css({
+				'background-image': 'url('+ this.currentBg[this.nowIndex] +')',
+			}).each(function (i, e) {
+				$(e).css ({
+					'left': i * _width,
+					'background-position': -i * _width,
+					'background-size': _parWidth
+				})
+			})
+		},
+		autoPlay: function () {
+			var _this = this;
+			clearInterval(this.timer);
+			this.timer = setInterval(function () {
+				_this.next();
+			}, 5000)
+		},
+		isAutoPlay: function () {
+			return this.timer;
+		},
+		stopAutoPlay: function () {
+			clearInterval(this.timer);
+			this.timer = null;
+		},
+		next: function (callback) {
+			// this.setting.isReady = false;
+			this.nowIndex ++;
+			this.nowIndex %= this.currentBg.length;
+			this.face.find('div').css({
+				'background-image': 'url('+ this.currentBg[this.nowIndex] +')',
+			});
+			this.showImage(callback);
+		},
+		prev: function () {
+			this.nowIndex --;
+			if (this.nowIndex < 0) {
+				this.nowIndex = this.currentBg.length-1;
+			}
+			// console.log(this.nowIndex)
+			this.face.find('div').css({
+				'background-image': 'url('+ this.currentBg[this.nowIndex] +')',
+			});
+			this.showImage();
+		},
+		hideImage: function () {
+			this.face.find('div').each(function(i, e){
+				dir = i%2 == 0 ? 1 : -1;
+				$(e).css ({
+					top:( dir * (tools.winSize().h * 0.4) * i/12 + dir * (tools.winSize().h * 0.2))
+				})
+			});
+			this.setting.showFn && this.setting.showFn();
+		},
+		isReady: function () {
+			return this.setting.isReady;
+		},
+		showImage: function (callback) {
+			this.setting.isReady = true;
+			var _this = this;
+			this.face.find('div').each(function (i, e) {
+				setTimeout(function (i) {
+					mTween(e, {top: 0, opacity: 100}, 500, 'easeOut', function () {
+						if (i == 11) {
+							_this.hideImage();
+							$('.face div').css ({opacity: 0});
+							$('.bg').css ({
+								'opacity': 1,
+							});
+							$('.bg div').css({
+								// 
+
+								'background-image': $('.face div').eq(0).css('background-image')
+							});
+
+							_this.setting.isReady = false;
+							callback && callback();
+						}
+						if (i == 0) {
+							$('.bg').animate ({
+								opacity: 0
+							});
+						}
+					});
+				}, i*70, i);
+			});
+			// console.log(_this.setting.title[_this.nowIndex]);
+			_this.setting.hideFn && _this.setting.hideFn(_this.setting.title[_this.nowIndex],_this.setting.infor[_this.nowIndex]);
+		}
+	};
+
+	// 主屏文字信息相关
+	function IntroBox (selector) {
+		this.obj = $(selector);
+		this.spanBox = this.obj.find('span');
+	}
+
+	IntroBox.prototype = {
+		constructor: IntroBox,
+		inite: function () {
+		},
+		showBox: function () {
+			this.obj.fadeIn(300);
+			this.spanBox.each(function (i, e) {
+				$(e).attr({
+					style: ''
+				})
+			})
+		},
+		hideBox: function (fn) {
+			this.obj.fadeOut(800, function () {
+				fn && fn();
+			});
+			this.spanBox.each(function (i, e) {
+				$(e).width(0);
+				$(e).height(0);
+			})
+		}
+	}
+
+	// 阻止移动端默认事件
+	document.addEventListener('touchstart', function (ev) {
+		ev.preventDefault();
+	})
+
+	var setImage = new setMainImage('#mainImagArea');
+	var mainIntroBox = new IntroBox('#centerBox .spanBox');
+
+	// 初始化：添加 进入 移出 的回调函数，将内容区 和 背景关联起来
+	setImage.init ({
+		showFn: function (title) {
+			mainIntroBox.showBox();
+			$('#title').fadeIn(800);
+			$('#intro').fadeIn(800);
+		},
+		hideFn: function (title, infor) {
+			mainIntroBox.hideBox();
+			$('#title').fadeOut(800, function () {
+				$('#title').html(title);
+			});
+			$('#intro').fadeOut(800, function () {
+				$('#intro p').html(infor)
+			});
+		}
+	});
+
+	// 初始化结构，顺便将将内容区 和 背景 一起创建结构，这里可以将setPos和init合并起来，将这里作为配置参数；
+	setImage.setPos(function (title, infor) {
+		$('#title').html(title);
+		$('#intro p').html(infor)
+	});
+
+	// 调用自动播放
+	setImage.autoPlay();
+
+	// 当窗口大小调整的时候，调整背景相关设置
+	window.onresize = function () {
+		setImage.rePos();
+	}
+
+	// 右边按钮，进入下一张
+	$('.rightBtn').click(function () {
+		if(setImage.isReady()){
+			return false;
+		};
+		setImage.stopAutoPlay();
+		setImage.next();
+		setImage.autoPlay();
+	});
+
+	// 左边按钮，进入下一张
+	$('.leftBtn').click(function () {
+		if(setImage.isReady()){
+			return false;
+		};
+		setImage.stopAutoPlay();
+		setImage.prev();
+		setImage.autoPlay();
+	});
+
+	centerBox.onclick = function (ev) {
+		ev.cancelBubble = true;
+	};
+
+
+
+
+
+	// menu 点击事件
+
+	var menuBox = new IntroBox('#menuBox .spanBox');
+	var execBox = new IntroBox('#execBox .spanBox');
+
+	var targetHash = 'menu';
+	var nowHash = 'main';
+
+	$('#header').on('click.menu', '.menu', function () {
+		if(setImage.isReady()){
+			return false;
+		};
+		$('.menu').toggleClass('active');
+		window.location.hash = targetHash;
+	})
+
+
+	// menu 下导航相关
+
+	$('#menuBox nav a').click(function () {
+		var _this = this;
+		$('#menuBox nav a').removeClass('active');
+		$(this).addClass('active');
+		$('.menu').toggleClass('active');
+		$('#menuBox .spanBox').animate({
+			top: $(_this).position().top
+		})
+	})
+
+	// 由于 menu 功能指向具有多样性，所以单独使用一个方法，进行区分
+
+	function backMenu () {
+		switch (nowHash) {
+			case 'main':
+				menuBox.hideBox();
+				$('#centerBox').fadeOut(800, function () {
+					$('#menuBox').fadeIn(400, function () {
+						menuBox.showBox();
+					});
+
+				});
+				setImage.stopAutoPlay();
+				setImage.tabBg(true);
+				setImage.next();
+				targetHash = 'main';
+				break;
+			case 'executive':
+				execBox.hideBox();
+				$('#execBox').fadeOut(800, function () {
+					$('#menuBox').fadeIn(400, function () {
+						menuBox.showBox();
+					});
+				});
+				targetHash = 'executive';
+				break;
+			default:
+				// statements_def
+				break;
+		}
+	}
+
+
+
+	// 子主题切换
+
+	window.onhashchange = function () {
+		var hash = ff.getHash();
+		switch (hash) {
+			case 'menu':
+				menuBox.hideBox();
+				$('#mainImagArea').addClass('bgBlue');
+				$('.menu').addClass('sonStyle');
+				backMenu();
+				window.location.hash = '';
+				break;
+			case 'main':
+				$('#mainImagArea').removeClass('bgBlue');
+				$('.menu').removeClass('sonStyle');
+				nowHash = 'main';
+				$('#menuBox').fadeOut(600,function () {
+					setImage.autoPlay();
+					setImage.tabBg(false);
+					setImage.next(function () {
+						$('#centerBox').fadeIn()
+					});
+				});
+				targetHash = 'menu';
+				window.location.hash = '';
+				break;
+			case 'executive' :
+				execBox.hideBox();
+				nowHash = 'executive';
+				$('.menu').addClass('sonStyle');
+				setImage.tabBg(true);
+				$('#menuBox').fadeOut(600);
+				setImage.next(function () {
+					$('#execBox').fadeIn(function () {
+						execBox.showBox();
+					})
+				});
+				window.location.hash = '';
+				targetHash = 'menu';
+				break;
+			default:
+				break;
+		}
+	}
+	var abcImg =  new setMainImage('#mainImagArea');
+	abcImg.init({
+		data: [
+			'img/bgWhite.jpg'
+		]
+	});
+	alert(1);
+	setImage.stopAutoPlay();
+	abcImg.next();
+})()
---
+++ resume/js/tool.js
@@ -0,0 +1,96 @@
+(function (global, factory) {
+	factory(global)
+})(typeof window !== 'undefined' ? window : this, function (global, noGlobal) {
+	function ffTools (selector) {
+		return new ffTools.prototype.inite(selector);
+	};
+
+	ffTools.prototype = {
+		constructor: ffTools,
+		inite: function (selector) {
+			this.obj = selector;
+		},
+		css: function () {
+			alert(1);
+		}
+	}
+
+
+
+	var tool = {
+		setTime:function  (time) {
+	        var data = new Date(time);
+	        var year = data.getFullYear();
+	        var month = data.getMonth() + 1;
+	        var week = data.getDate();
+	        var day = data.getDay();
+	        var h = data.getHours();
+	        var m = data.getMinutes();
+	        var s = data.getSeconds();
+	        return this.dateToString([
+	            year,
+	            this.turnToo(month),
+	            this.turnToo(week),
+	            this.turnToo(day),
+	            this.turnToo(h),
+	            this.turnToo(m),
+	            this.turnToo(s)
+	            ], 0)
+	    },
+	    turnToo: function (num) {
+	        num = num+'';
+	        return num.length == 1 ? "0" + num:num;
+	    },
+    	dateToString: function (arr,n) {
+	        //arr: 时间数组   ; n:时间连接方式
+	        var s = '';
+	        n = n || 0;
+	        var aStr = ['年月日时分秒','-- ::','// ::'];
+	        for ( var i=0; i<arr.length; i++ ) {
+	            s += arr[i]+aStr[n].charAt(i);
+	        };
+	        return s;
+	    },
+	    getHash: function () {
+			var lc = window.location.hash.substring(1);
+			// var data = {}
+			data = lc.split('=');
+			// data.name = lc[0];
+			// data.value = lc[1];
+			/*console.log(data);
+			if (data.length) {
+				return data[1];
+			} else {
+				return lc;
+			}*/
+			return data[1] ? data[1] : data[0];
+		},
+		getSearch: function () {
+			var lc = location.search.length > 0 ? location.search.substring(1) : '',
+			    data = {},
+			    items = lc.length ? lc.split('&') : [],
+			    item = null,
+			    name = null,
+			    value = null,
+			    i=0,
+			    len = items.length;
+			for (var i = 0; i < len; i++) {
+				item = items[i].split('=');
+				name = decodeURIComponent(item[0]);
+				value = decodeURIComponent(item[1]);
+				for (var i = 0; i < item.length; i++) {
+					data[name] = value;
+				}
+			}
+		return data;
+		}
+	}
+
+	for (attr in tool) {
+		ffTools[attr] = tool[attr];
+	}
+
+	ffTools.prototype.inite.prototype = ffTools.prototype;
+
+	window.ff = ffTools;
+})
